0306e3b1c7c27b8f6e8bed63f795b6c1835bc51f
smichelotti <88442082+smichelotti10@users.noreply.github.com>
2022-01-10 18:04:39 -0300

    Delete TetrisApplication directory

--- TetrisApplication/build/Debug/GNU-Linux/_ext/1ddc2790/escenario.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/GNU-Linux/_ext/1ddc2790/escenario.o: \
- /home/santiago/Documents/Code/TetrisGit/TetrisApplication/escenario.c
--- TetrisApplication/build/Debug/GNU-Linux/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-Linux/main.o: main.c rules.h piezas.h
-
-rules.h:
-
-piezas.h:

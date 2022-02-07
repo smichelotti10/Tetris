6e6094ea6bfb58554e7c1448baa41b0a366e99e0
EzequielDiazGuzman <72627168+EzequielDiazGuzman@users.noreply.github.com>
2022-02-07 19:26:22 -0300

    Update juego_allegro.h

--- TetrisApplication/juego_allegro.h
+++ TetrisApplication/juego_allegro.h
@@ -17,7 +17,8 @@ void print_siguiente_pieza (pieza_t* to_use, pieza_t* hold);
 void print_bloque_color (int pieza, float i, float j);
 void print_bordes_juego (void);
 void clear_display (void);
-void get_input (ALLEGRO_EVENT *event, ALLEGRO_EVENT_QUEUE *event_queue, ALLEGRO_SAMPLE*sound, pieza_t* in_use, pieza_t* hold, pieza_t* to_use, int mat[FIL][COL], char*end, ALLEGRO_FONT* font, game_stats_t* jugador);
+void get_input (ALLEGRO_EVENT *event, ALLEGRO_EVENT_QUEUE *event_queue, ALLEGRO_SAMPLE*sound, pieza_t* in_use, pieza_t* hold, pieza_t* to_use,
+                int mat[FIL][COL], char*end, ALLEGRO_FONT* font, game_stats_t* jugador);
 void itoa (int n, char s[]);
 
 #endif /* JUEGO_ALLEGRO_H */
